27e22dec7c55d93dea9ecefb7cb5ee343ce8c0e0
Brad Stimpson <brad.stimpson@gmail.com>
2016-10-30 22:37:23 -0400

    simple socketio code and new scene with no vr rig

---
+++ Assets/Network.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using SocketIO;
+
+
+public class Network : MonoBehaviour {
+
+	static SocketIOComponent socket;
+
+	public GameObject playerPrefab;
+
+	// Use this for initialization
+	void Start () {
+		socket = GetComponent<SocketIOComponent>();
+		socket.On("open", OnConnected);
+		socket.On("spawn", OnSpawned);
+	}
+
+	// Callback used when connected
+	void OnConnected(SocketIOEvent e) {
+		Debug.Log("connected");
+		socket.Emit("move");
+	}
+
+	void OnSpawned(SocketIOEvent e) { 
+		Debug.Log("another client connected");
+		Instantiate (playerPrefab);
+	}
+}
